dd73e7a4e0eb14d5d8c0ced9adf2edd0cff7ac5b
minhnhut1250@gmail.com <minhnhut1250@gmail.com>
2018-05-05 14:12:22 +0700

    loggggggggggin

---
+++ login/php/login.php
@@ -0,0 +1,42 @@
+<?php
+
+host="localhost";
+
+$username="root";
+
+$password="";
+
+$db_name="tacnghiep";
+
+$tbl_name="account";
+
+mysql_connect("$host", "$username", "$password")or die("Không thể kết nối");
+
+mysql_select_db("$db_name")or die("cannot select DB");
+
+$myusername=$_POST['username'];
+
+$mypassword=$_POST['password'];
+
+if(isset($_POST['dangnhap'])){
+    
+    $sql="SELECT * FROM $tbl_name WHERE username='$myusername' and password='$mypassword'";
+
+    $result=mysql_query($sql);
+
+    $count=mysql_num_rows($result);
+
+    if($count>0){
+
+    echo"<script>alert('<h3> Đăng nhập thành công !</h3>')</script>"
+
+    }
+
+    else {
+
+    echo "<script>alert('<h3>Sai tên đăng nhập hoặc mật khẩu</h3>')</script>";
+
+    }
+}
+
+?>
