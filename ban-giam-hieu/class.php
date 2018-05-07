0df6ba1ff328dd390cf80ee27813dde048845c8c
Huỳnh Khắc Duy <anminh113@gmail.com>
2018-05-07 20:38:11 +0700

    update class ban giám hiệu

--- ban-giam-hieu/class.php
+++ ban-giam-hieu/class.php
@@ -3,13 +3,17 @@
     $student = new Student();
     $getStudent = $student->getAll();
 
+    include_once("../database/model/Employee.php");
+    $emp = new Employee();
+    $getEmp = $emp->getAll();
 
     include_once("../database/model/Class_per.php");
     $class_per = new Class_per();    
     $getClass = $class_per->getAll();
     $getEmp_name = $class_per->getEMP_Name();
     $getStudent_Name = $class_per->getStudent_Name();
-    $countStudent_Name = $class_per->countStudent_Name();
+    $getStudent_all = $class_per->getStudent_all();
+
 
     
 ?>
@@ -17,13 +21,13 @@
     <link rel="stylesheet" href="../ban-giam-hieu/css/modal.css">
     <div class="container-fluid">
         <div class="row">
-            <div class="col-md-12">
+            <!-- <div class="col-md-12">
                 <button type="button" class="btn btn-secondary btn-lg" role="button" aria-disabled="true"
                         data-toggle="modal" data-target="#addModal-class">
                     <i class="material-icons">add_circle_outline</i>
                     Thêm lớp mới
                 </button>
-            </div>
+            </div> -->
             <div class="col-md-12">
                 <div class="card">
                     <div class="card-header card-header-primary"
@@ -50,9 +54,7 @@
                                 <th>
                                     Số lượng
                                 </th>
-                                <th>
-                                    Giới tính
-                                </th>
+                                
                                 <!-- <th class="text-center">
                                     Phòng
                                 </th> -->
@@ -85,37 +87,27 @@
                                     <td><?= $st["year"] ?></td>
                                     <td>
                                         <?php
-                                            $i=0;
-                                            foreach ($getStudent_Name as $stu) {
-                                            if($stu["class_id"] == $st["class_id"] && $stu["emp_id"] == $st["emp_id"])
-                                                {  
-                                                    $i++;
-                                                    $stu[number];
-                                                }
-                                            }
-                                            echo $i;
+                                        echo $class_per->getCount_student($st["class_id"]);
                                         ?>
                                     </td>
-                                    <td>
-                                        <?= $st["gender"] ?>
-                                    </td>
+                                    
                                     <!-- <td class="text-center">
                                         12B1
                                     </td> -->
                                     <td class="td-actions text-center">
-                                        <button type="button" rel="tooltip" class="btn btn-info btn-simple"
+                                        <button type="button" rel="tooltip" title="Thêm học sinh" class="btn btn-success btn-simple"
                                                 data-toggle="modal" data-target="#add-class">
                                             <i class="material-icons">add</i>
                                         </button>
-                                        <button type="button" rel="tooltip" class="btn btn-info btn-simple"
+                                        <button type="button" rel="tooltip" title="Xem chi tiết" class="btn btn-info btn-simple"
                                                 data-toggle="modal" data-target="#detail-class-<?= $st["class_id"] ?>">
                                             <i class="material-icons">remove_red_eye</i>
                                         </button>
-                                        <button type="button" rel="tooltip" class="btn btn-success btn-simple"
+                                        <!-- <button type="button" rel="tooltip" class="btn btn-success btn-simple"
                                                 data-toggle="modal" data-target="#edit-class-<?= $st["class_id"] ?>">
                                             <i class="material-icons">edit</i>
-                                        </button>
-                                        <button type="button" rel="tooltip" class="btn btn-danger btn-simple"
+                                        </button> -->
+                                        <button type="button" rel="tooltip" title="Xóa lớp" class="btn btn-danger btn-simple"
                                                 data-toggle="modal" data-target="#delete-class-<?= $st["class_id"] ?>">
                                             <i class="material-icons">close</i>
                                         </button>
@@ -152,11 +144,21 @@
                         <label for="exampleInput1" class="bmd-label-floating">Lớp</label>
                         <input type="text" name="lop" class="form-control" id="exampleInput1">
                     </div>
-                    <div class="form-group">
-                        <label for="exampleInput1" class="bmd-label-floating">Tên giáo viên</label>
-                        <input type="text" name="ten" class="form-control" id="exampleInput1">
+                    
+                    <div class="form-group ">
+                      <label for="inputState">Tên giáo viên</label>
+                      <select id="inputState" class="form-control">
+                        <option selected>Chọn giáo viên</option>
+                        <?php
+                            foreach ($getEmp as $e) {
+                        ?>
+                        <option><?= $e["emp_name"] ?></option>
+                        <?php
+                                }
+                                ?>
+                      </select>
                     </div>
-                    <div class="form-group">
+                    <!-- <div class="form-group">
                         <label for="exampleFormControlSelect1">Giới tính</label>
                         <div class="row">
                             <div class="col-lg-2">
@@ -183,7 +185,7 @@
                             </div>
 
                         </div>
-                    </div>
+                    </div> -->
                     <div class="form-group">
                         <label for="exampleInput1" class="bmd-label-floating">Năm học</label>
                         <input type="text" name="#" class="form-control" id="exampleInput1">
@@ -198,13 +200,14 @@
                         <input type="text" name="#" class="form-control" id="exampleInput1">
                     </div> -->
 
-                </form>
+                
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">ĐÓNG</button>
                 <span></span>
                 <button type="button" class="btn btn-primary">LƯU LẠI</button>
             </div>
+            </form>
         </div>
     </div>
 </div>
@@ -221,7 +224,7 @@
                 </button>
             </div>
             <div class="modal-body">
-                <form>
+                <form method="post">
                     <div class="form-group">
                         <label for="exampleInput1" class="bmd-label-floating">Danh sách học sinh</label>
                         <table class="table">
@@ -235,10 +238,10 @@
                                 <th>SĐT phụ huynh</th>
                                 <th>Địa chỉ</th>
                                 <th class="td-actions text-cente check-add" style="padding: 0px 8px 21px 0px">
-                                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                    
                                     <div class="form-check form-check-inline">
                                         <label class="form-check-label ">
-                                            <input class="form-check-input" type="checkbox" id="checkAll" value="option1">
+                                            <input class="form-check-input" type="checkbox" name="checkall" id="checkAll" value="option1">
                                             <span class="form-check-sign">
                                                     <span class="check"></span>
                                                 </span>
@@ -248,7 +251,7 @@
                             </thead>
                             <tbody>
                                 <?php
-                                    foreach ($getStudent as $s) {
+                                    foreach ($getStudent_all as $s) {
                                 ?>
                             <tr>
                                 <td class="text-center"><?= $s["student_id"] ?></td>
@@ -261,7 +264,7 @@
                                 <td class="td-actions text-right">
                                     <div class="form-check ">
                                         <label class="form-check-label check-add">
-                                            <input class="form-check-input" type="checkbox" id="checkItem" value="option1">
+                                            <input class="form-check-input" type="checkbox" name="<?= $s["student_id"] ?>" id="checkItem" value="option1">
                                             <span class="form-check-sign">
                                                 <span class="check"></span>
                                                 </span>
@@ -279,13 +282,14 @@
                         </table>
                     </div>
 
-                </form>
+                
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">ĐÓNG</button>
                 <span></span>
-                <button type="button" class="btn btn-primary">Thêm</button>
+                <button type="button" class="btn btn-primary" name="add-class">Thêm</button>
             </div>
+            </form>
         </div>
     </div>
 </div>
@@ -322,7 +326,7 @@
                                 }
                             }
                     ?>        
-                    <div class="form-group">
+                    <!-- <div class="form-group">
                         <label for="exampleFormControlSelect1">Giới tính</label>
                         <div class="row">
                             <div class="col-lg-2 col-md-2">
@@ -364,7 +368,7 @@
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    </div> -->
                     <div class="form-group">
                         <label for="exampleInput1" class="bmd-label-floating">Năm học</label>
                         <input class="form-control" type="text" value="<?= $st["year"]?>" readonly="readonly">
@@ -373,15 +377,7 @@
                     <div class="form-group">
                         <label for="exampleInput1" class="bmd-label-floating">Số lượng học sinh</label>
                         <input class="form-control" type="text" value="<?php
-                                            $i=0;
-                                            foreach ($getStudent_Name as $stu) {
-                                            if($stu["class_id"] == $st["class_id"] && $stu["emp_id"] == $st["emp_id"])
-                                                {  
-                                                    $i++;
-                                                    $stu[number];
-                                                }
-                                            }
-                                            echo $i;
+                                        echo $class_per->getCount_student($st["class_id"]);
                                         ?>" readonly="readonly">
                     </div>
 
@@ -407,7 +403,8 @@
                                 <?php
                                     $i=0;
                                     foreach ($getStudent_Name as $stu) {
-                                    if($stu["class_id"] == $st["class_id"] && $stu["emp_id"] == $st["emp_id"]){?>
+                                        if($st["class_id"] == $stu["class_id"])
+                                    {?>
                             <tr>
                                 <td class="text-center"><?= $stu["student_id"] ?></td>
                                 <td><?= $stu["student_name"] ?></td>
@@ -452,18 +449,28 @@
                             <label for="exampleInput1" class="bmd-label-floating">Lớp</label>
                             <input type="text" name="lop" class="form-control" value="<?= $st["class_name"] ?>" id="exampleInput1">
                         </div>
-                        <?php
-                        foreach ($getEmp_name as $emp) {
-                            if($emp["class_id"] == $st["class_id"]){?>
-                                <div class="form-group">
-                                    <label for="exampleInput1" class="bmd-label-floating">Tên giáo viên</label>
-                                    <input type="text" name="ten" class="form-control" value="<?= $emp["emp_name"] ?>" id="exampleInput1">
-                                </div>
-                                <?php  
+
+                        <div class="form-group ">
+                            <?php
+                            foreach ($getEmp_name as $emp) {
+                                if($emp["class_id"] == $st["class_id"]){?>
+                                    <label for="inputState">Tên giáo viên</label>
+                                    <select id="inputState" class="form-control">
+                                    <option selected><?= $emp["emp_name"] ?></option>    
+                            <?php
+                            foreach ($getEmp as $e) {
+                            ?>
+                            <option><?= $e["emp_name"] ?></option>
+                            <?php
+                            }
+                            ?>
+                                    </select>
+                           <?php  
                                 }
                             }
-                        ?>
-                        <div class="form-group">
+                            ?>
+                        </div>
+                        <!-- <div class="form-group">
                             <label class="bmd-label-floating">Giới tính</label>
                             <div class="form-check form-check-radio">
                                 <?php if($s["gender"] == "Nam"){ ?>
@@ -498,7 +505,7 @@
                                 </label>
                                 <?php } ?>
                             </div>
-                        </div>
+                        </div> -->
                         <div class="form-group">
                             <label for="exampleInput1" class="bmd-label-floating">Năm học</label>
                             <input type="" name="#" class="form-control" value="<?= $st["year"] ?>" id="exampleInput1">
@@ -551,6 +558,58 @@
 <?php
     }
 ?>
+
+                <?php
+                if (isset($_POST["add-class"])) {
+                    for ($j=0; $j < count($_POST['lol']);$j++) 
+                        {
+                    $insert=mysql_query("INSERT INTO castvote (candidate) VALUES '".$_POST['lol'][$j]."')");
+                    }
+
+                    
+                    if($check){
+                        echo "<script>alertAdd(true,'Đã thêm <b>".$name."</b> thành công!');</script>";
+                    }
+                    else{
+                        echo "<script>alertAdd(false,'Thêm học sinh thất bại!');</script>";
+                    }
+                }
+                // if (isset($_POST["update-student"])) {
+                //     $id = $_POST["id"];
+                //     $name = $_POST["name"];
+                //     $dob = $_POST["dob"];
+                //     $gender = $_POST["gender"];
+                //     $hometown = $_POST["hometown"];
+                //     $address = $_POST["address"];
+                //     $current_address = $_POST["current_address"];
+                //     $father_name = $_POST["father_name"];
+                //     $father_job = $_POST["father_job"];
+                //     $father_phone = $_POST["father_phone"];
+                //     $mother_name = $_POST["mother_name"];
+                //     $mother_job = $_POST["mother_job"];
+                //     $mother_phone = $_POST["mother_phone"];
+
+                //     $check = $student->update($id, $name, $dob, $gender, $hometown, $address, $current_address, $father_name, $father_job, $father_phone, $mother_name, $mother_job, $mother_phone);
+
+                //     if($check){
+                //         echo "<script>alertEdit(true,'Đã sửa <b>".$name."</b> thành công!');</script>";
+                //     }
+                //     else{
+                //         echo "<script>alertEdit(false,'Sửa thông tin học sinh thất bại!');</script>";
+                //     }
+                // }
+                // if (isset($_POST["delete-student"])) {
+                //     $id = $_POST["id"];
+                //     $check = $student->delete($id);
+                //     if($check){
+                //         echo "<script>alertDelete(true,'Xoá <b>".$name."</b> thành công!');</script>";
+                //     }
+                //     else{
+                //         echo "<script>alertDelete(false,'Xoá học sinh thất bại!');</script>";
+                //     }
+                // }
+
+                ?>
 
 <!--    End Modal-->
 <script>
