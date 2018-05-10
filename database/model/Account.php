dbc629324d26c3c18a309cd63df79569051d1973
Truong Duc Huy <huyb1400759@gmail.com>
2018-05-10 11:28:42 +0700

    xong model account

--- database/model/Account.php
+++ database/model/Account.php
@@ -1,5 +1,7 @@
 <?php
 include_once("Database.php");
+
+
 class Account extends Database
 {
     private $table = "account";
@@ -7,6 +9,7 @@ class Account extends Database
     private $pwd;
     private $emp_id;
     private $per_name;
+
     public function getAll()
     {
         $data = array();
@@ -17,6 +20,7 @@ class Account extends Database
         }
         return $data;
     }
+
     public function getOne($emp_id)
     {
         $sql = "SELECT * FROM $this->table WHERE emp_id = $emp_id";
@@ -24,20 +28,22 @@ class Account extends Database
         $data[] = $q->fetch_assoc();
         return $data;
     }
+
     public function getAccountDetail()
     {
         $data = array();
-        $sql = "SELECT employee.emp_id, employee.emp_name, username, password, permission.per_name 
-                FROM employee
-                JOIN account ON employee.emp_id = account.emp_id
-                JOIN permission ON account.per_id = permission.per_id";
+            $sql = "SELECT employee.emp_id, employee.emp_name, username, permission.per_name, `password`, permission.per_id 
+            FROM employee
+            JOIN account ON employee.emp_id = account.emp_id
+            JOIN permission ON account.per_id = permission.per_id
+            ORDER BY permission.per_id;";
         $q = $this->conn->query($sql) or die ("failed!");
         while ($r = $q->fetch_assoc()) {
-            array_push($data, $r);
+                array_push($data, $r);
             }
-        }
         return $data;
     }
+
     public function login($username, $password)
     {
         $sql = "SELECT employee.*, permission.* FROM `account` " .
@@ -48,34 +54,37 @@ class Account extends Database
         $data = $q->fetch_assoc();
         return $data;
     }
+
     public function delete($username)
     {
-        $query = "DELETE FROM $this->table WHERE username = '$username'";
+        $query = "DELETE FROM $this->table WHERE `username` = '$username'";
         $stmt = $this->conn->query($query);
-        if ($stmt == false) echo "<script>alert('Delete failed')</script>";
-        return $stmt == true;
+        echo "<script>$query</script>";
+        if ($stmt === false) echo "<script>alert('Delete failed')</script>";
+        return $stmt === true;
     }
+
     public function insert($username, $password, $emp_id, $per_id)
     {
         $query = "INSERT INTO `account` (`username`, `password`, `emp_id`, `per_id`) VALUES ('$username', '$password', '$emp_id', '$per_id')";
         $stmt = $this->conn->query($query);
-        // echo "<script>alert('". $query . " " . $stmt ."')</script>";
+        echo "<script>alert('". $query . " " . $stmt ."')</script>";
         if ($stmt === false) echo "<script>alert('Insert failed')</script>";
         return $stmt === true;
     }
+
     public function update($emp_id, $username, $password, $per_id)
     {
-        $query = "UPDATE $this->table SET  `username`= '$username', `password`= '$password', `per_id` = $per_id WHERE `emp_id` = '$emp_id'";
+        $query = "UPDATE $this->table SET  `username`= '$username', `password`= '$password', `per_id` = '$per_id' WHERE `emp_id` = '$emp_id'";
         $stmt = $this->conn->query($query);
-        return $stmt == true;
+        return $stmt === true;
     }
     public function getEmpList(){
         $data = array();
-        $sql = "SELECT `emp_id`, `emp_name` FROM `employee` WHERE emp_id NOT IN (SELECT `emp_id` FROM `account`)";
+        $sql = "SELECT `emp_id`, `emp_name` FROM `employee`  WHERE emp_id NOT IN (SELECT `emp_id` FROM `account`)";
         $q = $this->conn->query($sql) or die("failed");
-        $data = $q->fetch_assoc();
         while ($r = $q->fetch_assoc()) {
-            array_push($data,$r);
+           array_push($data,$r);
         }
         return $data;
     }
@@ -92,4 +101,5 @@ class Account extends Database
         return $stmt == true;
     }
 }
+
 ?>
