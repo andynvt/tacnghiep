dcc28518058de1796f9a98b7a2e453447c88ccc8
ANDY <andy.nvt.vn@gmail.com>
2018-05-10 10:18:08 +0700

    xoá modal logout

--- ban-giam-hieu/student.php
+++ ban-giam-hieu/student.php
@@ -485,25 +485,6 @@ $getStudent = $student->pagination(10,$_GET['page']);
     <?php
     }
     ?>
-    <div class="modal fade" id="logout" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-         aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h3 class="modal-title">Bạn có muốn đăng xuất?</h3>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">ĐÓNG</button>
-                    <span></span>
-                    <button type="button" class="btn btn-primary">CHẤP NHẬN</button>
-                </div>
-            </div>
-        </div>
-    </div>
 </div>
 
 
