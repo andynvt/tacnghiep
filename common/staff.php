ff6b5009d5b3693c35d2498ac33f11631913eec3
minhnhut1250@gmail.com <minhnhut1250@gmail.com>
2018-05-10 23:10:25 +0700

    đưa vô staff.php theo yêu cầu chuna

--- common/staff.php
+++ common/staff.php
@@ -1,4 +1,3 @@
-
 <div class="modal fade" id="addModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
@@ -157,6 +156,68 @@
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">ĐÓNG</button>
                     <span></span>
                     <button type="submit" class="btn btn-primary">CHẤP NHẬN</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="modal fade" id="changePass" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h3 class="modal-title" id="exampleModalLabel">Đổi mật khẩu</h3>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form method="post">
+                    <div class="form-group">
+                        <label for="exampleInput1" class="bmd-label-floating">Ten tai khoan</label>
+                        <input type="text" class="form-control" id="" name="username" value="<?= $account['username']?>" readonly="readonly" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInput1" class="bmd-label-floating">Mật khẩu cũ</label>
+                        <input type="password" class="form-control" id="" name="oldPass" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleFormControlSelect1">Mật khẩu mới</label>
+                        <input type="password" class="form-control" id="" name="newPass" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInput1" class="bmd-label-floating">Nhập lại mật khẩu</label>
+                        <input type="password" class="form-control" id="" name="passwordconfirm" required>
+
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">ĐÓNG</button>
+                        <span></span>
+                        <button type="submit" name="chane" class="btn btn-primary">Lưu Lại</button>
+                    </div>
+                </form>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="modal fade" id="question" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h3 class="modal-title">Mật khẩu đã được thay đổi !<br> Bạn có muốn đăng xuất không ?</h3>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-footer">
+                <form action="../login/php/logout.php" method="post">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">ĐÓNG</button>
+                    <span></span>
+                    <button type="submit" name="confirm" class="btn btn-primary">CHẤP NHẬN</button>
                 </form>
             </div>
         </div>
